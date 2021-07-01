668e10470f62662681a9e2aa1de9eca2fcdfaff6
Ken Lowrie <klowrie@gmail.com>
2021-07-01 17:24:50 -0500

    Update sql.logininfo.php
    Abstracted the hostname, since my new EC2 instance running plesk uses mariadb instead of mysql and needs different hostname.

--- src/auth/sql.logininfo.php
+++ src/auth/sql.logininfo.php
@@ -1,5 +1,10 @@
 <?php
 
+function db_hostname()
+{
+    return "localhost:3306";    // default for mariadb on my server. dev machine uses '127.0.0.1'. update as required...
+}
+
 function db_name()
 {
     return "YOUR_DB_NAME_HERE";
