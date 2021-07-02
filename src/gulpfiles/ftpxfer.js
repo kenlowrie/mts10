dcc97bb66ba19571afd6251607c4cd8906a16571
Ken Lowrie <klowrie@gmail.com>
2021-07-02 11:11:58 -0500

    Update ftpxfer.js
    Have it xfer the logininfo files before uploading. Make it so it only uploads the file that has changed instead of everything.

--- src/gulpfiles/ftpxfer.js
+++ src/gulpfiles/ftpxfer.js
@@ -4,15 +4,10 @@ var gulp = require('gulp');
 var sftp = require( 'gulp-sftp-up4' );
 var fs = require('fs-extra');
 
-/** Configuration **/
-var host = process.env.FTP_HOST;  
-var auth = 'privateKeyCustom';
-var authFile = '../../.ftppass';
-var localFilesGlob = ['**/*', '!ftpxfer.js', '!phpserver.js'];  
+// allow overriding the login info creds via files in root folder...
+function checkForAuthOverride(authFilename) {
 
-/*function checkForFTPPassOverride(ftpPassFileName) {
-
-	var sourceName = '../../' + ftpPassFilename;
+	var sourceName = '../../' + authFilename;
 	
 	try {
 		fs.copySync(sourceName, authFilename)
@@ -20,14 +15,23 @@ var localFilesGlob = ['**/*', '!ftpxfer.js', '!phpserver.js'];
 	} catch (err) {
 		console.error("Autho override for " + authFilename + " failed.\r\n" + err);
 	}
-}*/
+}
 
+function handleOverrides(){
+	checkForAuthOverride('users.logininfo.php');
+	checkForAuthOverride('sql.logininfo.php');
+}
 
-//var remoteFolder = '/public_html/sd/mts10'
+/** Configuration **/
+var host = process.env.FTP_HOST;  
+var auth = 'privateKeyCustom';
+var authFile = '../../.ftppass';
+var localFilesGlob = ['**/*', '!ftpxfer.js', '!phpserver.js'];  
+
 var remoteFolder = '/var/www/vhosts/klowrie.net/httpdocs/sd/mts10'
 
-function deploy(){
-    return gulp.src(localFilesGlob, {base: '.', buffer: false })
+function deploy(files){
+    return gulp.src(files, {base: '.', buffer: false })
         .pipe( sftp({
             host: host,
             auth: auth,
@@ -39,25 +43,27 @@ function deploy(){
  * Deploy task.
  * Copies the new files to the server
  *
- * Usage: `FTP_USER=someuser FTP_PWD=somepwd gulp ftp-deploy`
+ * Usage: `FTP_HOST=some.ip.add.r gulp -f ftpxfer ftp-deploy`
  */
 gulp.task('ftp-deploy', function() {
-    return deploy();
+    handleOverrides();
+    return deploy(localFilesGlob);
 });
 
 /**
  * Watch deploy task.
  * Watches the local copy for changes and copies the new files to the server whenever an update is detected
  *
- * Usage: `FTP_HOST=some.ip.add.r gulp ftp-deploy-watch`
+ * Usage: `FTP_HOST=some.ip.add.r gulp -f ftpxfer ftp-deploy-watch`
  */
 gulp.task('ftp-deploy-watch', function() {
 
+    handleOverrides();
     gulp.watch(localFilesGlob)
-    .on('change', function(event) {
-      console.log('Changes detected! Uploading file "' + event.path + '", ' + event.type);
+    .on('change', function(path, stats) {
+      console.log('Changes detected! Uploading file "' + path);
 
-      return deploy();
+      return deploy(path);
     });
 });
 
